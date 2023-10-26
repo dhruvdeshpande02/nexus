4be57e96ccfd775d8a0437fcee1c6da793899037
dhruvdeshpande02 <101721546+dhruvdeshpande02@users.noreply.github.com>
2023-10-27 02:04:59 +0530

    Delete portal/nexus directory

--- portal/nexus/admin.py
+++
@@ -1,9 +0,0 @@
-# admin.py
-from django.contrib import admin
-from .models import User,Student,Coordinator,TNPAdmin
-
-admin.site.register(User)
-admin.site.register(Student)
-admin.site.register(Coordinator)
-admin.site.register(TNPAdmin)
-
--- portal/nexus/forms.py
+++
@@ -1,9 +0,0 @@
-# forms.py
-
-from django import forms
-from .models import Coordinator
-
-class CoordinatorCreationForm(forms.ModelForm):
-    class Meta:
-        model = Coordinator
-        fields = ['F_NAME', 'F_PHONE_NO', 'F_EMAIL', 'F_DEPARTMENT', 'F_Personal_Email', 'F_ID', 'F_INSTITUTE']
